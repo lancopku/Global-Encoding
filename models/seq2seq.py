8e4b841fcad1fb3b8b09549db32bdb06a98ee088
Justin Lin <justinlin930319@hotmail.com>
2018-05-24 14:10:41 +0800

    Update seq2seq.py

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -1,10 +1,3 @@
-'''
- @Date  : 2017/12/18
- @Author: Shuming Ma
- @mail  : shumingma@pku.edu.cn 
- @homepage: shumingma.com
-'''
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
