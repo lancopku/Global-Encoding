70c74a01ff4dec62ff1f74712b1c43cea0194268
Justin Lin <justinlin930319@hotmail.com>
2018-07-22 21:33:05 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -81,24 +81,3 @@ def rouge(reference, candidate, log_path, print_log, config):
               % (str(f_score), str(recall), str(precision)))
 
     return f_score[:], recall[:], precision[:]
-
-if __name__ == "__main__":
-    with codecs.open('/home/linjunyang/giga/valid.src', 'r', 'utf-8') as fc, codecs.open('/home/linjunyang/giga/valid.tgt', 'r', 'utf-8') as fr:
-        cand, ref = fc.readlines(), fr.readlines()
-        ref_ = []
-        for r in ref:
-            ref_.append(r.split())
-        can = []
-        for c in cand:
-            can.append(c.split()[:9])
-    print(rouge(ref_, can, log_path='/home/linjunyang/'))
-    '''with codecs.open('/home/linjunyang/giga/train.src', 'r', 'utf-8') as fc, codecs.open('/home/linjunyang/lcsts/train.tgt', 'r', 'utf-8') as fr, codecs.open('/home/linjunyang/giga/train.src', 'r', 'utf-8') as gc, codecs.open('/home/linjunyang/giga/train.tgt', 'r', 'utf-8') as gr:
-        cand, ref, gcand, gref = fc.readlines(), fr.readlines(), gc.readlines(), gr.readlines()
-    lsrc, ltgt, giga_src, giga_tgt = [], [], [], []
-    for i in range(len(ref)):
-        lsrc.append(len(cand[i].split()))
-        ltgt.append(len(ref[i].split()))
-        giga_src.append(len(gcand[i].split()))
-        giga_tgt.append(len(gref[i].split()))
-    print(min(lsrc), max(lsrc), sum(lsrc)/len(lsrc), min(ltgt), max(ltgt), sum(ltgt)/len(ltgt))
-    print(min(giga_src), max(giga_src), sum(giga_src)/len(giga_src), min(giga_tgt), max(giga_tgt), sum(giga_tgt)/len(giga_tgt))'''
